0a5ca5bc0d15fac6a3be7d2d269b420d9b735f90
Focusbe <focusbe@outlook.com>
2018-12-30 11:20:17 +0800

    同步代码

--- backend/node/bin/qihu.js
+++ backend/node/bin/qihu.js
@@ -11,7 +11,7 @@ class Qihu {
             area: "地区",
             categories: "类型",
             isover: "剧集",
-            emcee:"主持"
+            emcee: "主持"
         };
     }
     _getInfoIndex(title) {
@@ -22,25 +22,25 @@ class Qihu {
         }
         return false;
     }
-    
-    async _getjujilist(jujilist,year,source){
+
+    async _getjujilist(jujilist, year, source) {
         var videos = {};
         videos['list'] = [];
-        await utli.asynceach(jujilist,function(index,item){
+        await utli.asynceach(jujilist, function (index, item) {
             item = cheerio(item)
             //console.log(cheerio(item) .html());
             var title = item.find('.title').text();
             var playurl = item.attr('href');
-            if(playurl){
+            if (playurl) {
                 playurl = utli.getClearUrl(playurl);
             }
             var date = item.find(".w-newfigure-hint").text();
             var poster = item.find(".js-playicon>img").attr('src');
-            if(date){
-                date = utli.getDate(date,'-');
+            if (date) {
+                date = utli.getDate(date, '-');
             }
-            if(playurl&&title){
-                var video = {title:title,url:playurl,source:source,publishtime:date,poster:poster}
+            if (playurl && title) {
+                var video = { title: title, url: playurl, source: source, publishtime: date, poster: poster }
                 videos['list'].push(video);
                 videos['year'] = year;
                 videos['source'] = source;
@@ -51,58 +51,58 @@ class Qihu {
         });
         return videos;
     }
-    async _getPlayListByDetail(idstr){
+    async _getPlayListByDetail(idstr) {
         var self = this;
         var para = idstr.split("_");
-        if(para.length!=4){
+        if (para.length != 4) {
             return false;
         }
         //console.log('https://www.360kan.com/va/'+para[2]+'.html');
         var detailhtml = await utli._request({
-            host:'www.360kan.com',
-            path:'/va/'+para[2]+'.html'
-        },'GET',{});
-        if(detailhtml){
+            host: 'www.360kan.com',
+            path: '/va/' + para[2] + '.html'
+        }, 'GET', {});
+        if (detailhtml) {
             var $ = cheerio.load(detailhtml);
             var yearlist = $("#js-year .js-year-list>li>a");
             var playlist = [];
             var jujilist;
             var videos;
-            if(yearlist.length>0){
-                await utli.asynceach(yearlist,async function(index,item){
+            if (yearlist.length > 0) {
+                await utli.asynceach(yearlist, async function (index, item) {
                     var year = parseInt($(item).text());
-                    
-                    if(year){
-                        var jujibyyear = await utli._request({host:'www.360kan.com',path:'/cover/zongyilist'},'get',{id:para[2],do:'switchyear','year':year,site:para[3]});
+
+                    if (year) {
+                        var jujibyyear = await utli._request({ host: 'www.360kan.com', path: '/cover/zongyilist' }, 'get', { id: para[2], do: 'switchyear', 'year': year, site: para[3] });
                         var juji = cheerio.load(JSON.parse(jujibyyear)['data']);
                         jujilist = juji('#js-year-all>ul>li>a');
-                        videos = await self._getjujilist(jujilist,year,para[3]);
+                        videos = await self._getjujilist(jujilist, year, para[3]);
                     }
                     //console.log({id:para[2],do:'switchyear','year':parseInt($(item).html()),site:para[3]});
                 });
 
             }
-            else{
+            else {
                 //console.log($('#js-year-all>ul>li>a'));
-                
+
                 jujilist = $('#js-year-all>ul>li>a');
-                videos = await self._getjujilist(jujilist,utli.getcurYear(),para[3]);
+                videos = await self._getjujilist(jujilist, utli.getcurYear(), para[3]);
             }
             return videos;
-            
+
         }
         //console.log(detailhtml);
     }
-    async _getPlayList(idstr){
-        if(!idstr){
+    async _getPlayList(idstr) {
+        if (!idstr) {
             return false;
         }
         var para = idstr.split("_");
         // console.log(para);
-        if(para[1]==2||para[1]==4){
+        if (para[1] == 2 || para[1] == 4) {
             return this._getPlayListById(idstr);
         }
-        else if(para[1]==3){
+        else if (para[1] == 3) {
             return this._getPlayListByDetail(idstr);
         }
     }
@@ -126,16 +126,16 @@ class Qihu {
                 var $ = cheerio.load(result[0].seriesHTML);
                 var textarea = $("textarea").html();
                 var links;
-                if(!textarea){
+                if (!textarea) {
 
                     links = $(".js-series-item a");
                 }
-                else{
+                else {
                     textarea = utli.HTMLDecode(textarea);
                     $ = cheerio.load(textarea);
                     links = $('a');
                 }
-                links.each(function() {
+                links.each(function () {
                     playurl.push({
                         title: $(this).html(),
                         url: $(this).attr("href")
@@ -159,60 +159,60 @@ class Qihu {
         var $ = cheerio.load(html);
         var long_item = $("#js-longvideo>.js-longitem,#js-longvideo>.js-dianshi");
         var albums = [];
-        
-        albums = await utli.asynceach(long_item, async function(index, item) {
+
+        albums = await utli.asynceach(long_item, async function (index, item) {
             var album = {};
             var img = $(item)
                 .find(".m-mainpic img")
                 .attr("src");
             var contentWrap = $(item).children(".cont");
-            
+
             album["detailurl"] = $(item)
                 .find(".m-mainpic>a")
                 .attr("href");
-            
+
 
             var tabitems = $(item).find(
                 ".playsiteWrap a"
             );
             console.log(tabitems.length);
             album["playlist"] = [];
-            if( $(item).find(
-                ".b-series-item").length>0){
-            	console.log('是剧集');
-                await utli.asynceach(tabitems, async function(
+            if ($(item).find(
+                ".b-series-item").length > 0) {
+                console.log('是剧集');
+                await utli.asynceach(tabitems, async function (
                     index,
                     item
                 ) {
-                    
+
                     var playlist = await self._getPlayList($(item).data("tab"));
-                    if(playlist){
+                    if (playlist) {
                         album['playlist'].push(playlist);
                     }
                     return playlist;
                 });
             }
-            else{
-            	console.log('不是剧集');
+            else {
+                console.log('不是剧集');
                 //tabitems = $(item).find(".index-dianying-playsite a,.dropdown>.dropmenu>a,.playsiteWra>.b-playsite>a");
-                await utli.asynceach(tabitems, async function(
+                await utli.asynceach(tabitems, async function (
                     index,
                     item
                 ) {
-                    
+
                     var playurl = $(item).data('url');
                     console.log(playurl);
-                    var title = $(item).attr('data-daochu').replace('to=','');
+                    var title = $(item).attr('data-daochu').replace('to=', '');
                     var playerlist = {};
-                    playerlist['list'] = [{title:'1',url:playurl}];
-                    playerlist['source'] = title; 
-                    if(playurl){
+                    playerlist['list'] = [{ title: '1', url: playurl }];
+                    playerlist['source'] = title;
+                    if (playurl) {
                         album['playlist'].push(playerlist);
                     }
                     return playurl;
                 });
             }
-            
+
             album["title"] = contentWrap.find(".title>a").text();
             album["img"] = img;
             album["playtype"] = contentWrap.find(".title>.playtype").text();
@@ -222,7 +222,7 @@ class Qihu {
                     .replace("]", "");
             }
             var info_ul = contentWrap.find("ul>li");
-            info_ul.each(function() {
+            info_ul.each(function () {
                 var curtitle = $(this)
                     .children("b")
                     .text();
@@ -236,15 +236,19 @@ class Qihu {
                         .remove();
                     var infodetail = $(this).children("*");
                     var infoarr = [];
-                    infodetail.each(function() {
+                    infodetail.each(function () {
                         infoarr.push($(this).text());
                     });
                     album[curIndex] = infoarr;
                 }
             });
-            album["desc"] = contentWrap
+            let descstr = contentWrap
                 .find(".m-description>p")
-                .text().replace('简','');
+                .text();
+
+            descstr = descstr.split('：');
+            album['desc'] = descstr[descstr.length-1]
+            
             return album;
         });
         return albums;
--- backend/node/test.js
+++ backend/node/test.js
@@ -2,5 +2,8 @@ const Qihu = require('./bin/qihu');
 var qihu = new Qihu();
 qihu.search("吐槽大会").then((res)=>{
     console.log(res);
+    for(var i in res[0]['playlist']){
+        console.log(i);
+    }
     //console.log(res[0]['playlist']);
 });
